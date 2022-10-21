47e6ff08c5e9feef9eefee5b08cee854db95da29
ChristianAguero <christian.aguero.aguero@potros.itson.edu.mx>
2022-10-21 12:46:10 -0700

    Cambios a Operacion y al Main

--- src/mx/itson/catrina/negocio/Operacion.java
+++ src/mx/itson/catrina/negocio/Operacion.java
@@ -15,13 +15,114 @@ import mx.itson.catrina.entidades.Movimiento;
  */
 public class Operacion {
     
+    int totalEnero = 0;
+    int totalFebrero = 0;
+    int totalMarzo = 0;
+    int totalAbril = 0;
+    int totalMayo = 0;
+    int totalJunio = 0;
+    int totalJulio = 0;
+    int totalAgosto = 0;
+    int totalSeptiembre = 0;
+    int totalOctubre = 0;
+    int totalNoviembre = 0;
+    int totalDiciembre = 0;
+    
     public List<Movimiento> obtenerMovimientos (String mes, List<Movimiento> movimientos){
         
         DateFormat formato = new SimpleDateFormat("MM");
+        DateFormat formato2 = new SimpleDateFormat("dd/MM/yy");
+        DateFormat formato3 = new SimpleDateFormat("dd");
         
         List<Movimiento> movimientosMes = null;
         
         Movimiento movimientosOperacion = new Movimiento();
+        
+        
+        
+         for(Movimiento m : movimientos){
+             
+             switch (mes) {
+            
+                case "Enero":
+
+                    
+
+                    break;
+
+                case "Febrero":
+
+
+
+                    break;
+
+                case "Marzo":
+
+                    
+
+                    break;
+
+                case "Abril":
+
+                    
+
+                    break;
+
+                case "Mayo":
+
+                   
+
+                    break;
+
+                case "Junio":
+
+                    
+
+                    break;
+
+                case "Julio":
+
+                    
+
+                    break;
+
+                case "Agosto":
+
+                    
+
+                    break;
+
+                case "Septiembre":
+
+                    
+
+                    break;
+
+                case "Octubre":
+
+                    
+
+                    break;
+
+                case "Noviembre":
+
+                    
+
+                    break;
+
+                case "Diciembre":
+
+                     
+
+                    break;
+
+                    default:
+
+                        break;
+
+                    }
+             
+         }
         
         switch (mes) {
             
@@ -31,8 +132,34 @@ public class Operacion {
                 
                     if(formato.format(movimientosOperacion.getFecha()).equals("01")){
                     
-                    
-                        
+                        //Ingrediente ingrediente1 = new Ingrediente();
+                       // ingrediente1.setNombre("1 Taza de Lentejas previamente remojadas por 3 horas y escurridas");
+                       // ingredientes.add(ingrediente1);
+                       //receta.setIngredientes(ingredientes);
+                       
+                       Movimiento movimiento1 = new Movimiento();
+                       movimiento1.setDescripcionOperacion(movimientosOperacion.getDescripcion());
+                       movimiento1.setFechaOperacion(movimientosOperacion.getFecha());
+                       
+                       switch(movimientosOperacion.getTipo()){
+                           
+                           case DEPÓSITO:
+                               
+                               
+                               
+                               break;
+                          
+                           case RETIRO:
+                               
+                               
+                               
+                               break;
+                               
+                           default:
+                               
+                               break;
+                           
+                       }
                         
                     }
                         
@@ -44,9 +171,8 @@ public class Operacion {
                 
                 for(Movimiento m : movimientos){
                 
-                    if(formato.format(movimientosOperacion.getFecha()).equals("01")){
-                    
-                    
+                    if(formato.format(movimientosOperacion.getFecha()).equals("02")){
+                        
                         
                         
                     }
@@ -59,9 +185,8 @@ public class Operacion {
                 
                 for(Movimiento m : movimientos){
                 
-                    if(formato.format(movimientosOperacion.getFecha()).equals("01")){
-                    
-                    
+                    if(formato.format(movimientosOperacion.getFecha()).equals("03")){
+                        
                         
                         
                     }
@@ -74,8 +199,7 @@ public class Operacion {
                 
                 for(Movimiento m : movimientos){
                 
-                    if(formato.format(movimientosOperacion.getFecha()).equals("01")){
-                    
+                    if(formato.format(movimientosOperacion.getFecha()).equals("04")){
                     
                         
                         
@@ -89,9 +213,8 @@ public class Operacion {
                 
                for(Movimiento m : movimientos){
                 
-                    if(formato.format(movimientosOperacion.getFecha()).equals("01")){
-                    
-                    
+                    if(formato.format(movimientosOperacion.getFecha()).equals("05")){
+
                         
                         
                     }
@@ -104,9 +227,8 @@ public class Operacion {
                 
                 for(Movimiento m : movimientos){
                 
-                    if(formato.format(movimientosOperacion.getFecha()).equals("01")){
-                    
-                    
+                    if(formato.format(movimientosOperacion.getFecha()).equals("06")){
+                     
                         
                         
                     }
@@ -119,8 +241,7 @@ public class Operacion {
                 
                 for(Movimiento m : movimientos){
                 
-                    if(formato.format(movimientosOperacion.getFecha()).equals("01")){
-                    
+                    if(formato.format(movimientosOperacion.getFecha()).equals("07")){
                     
                         
                         
@@ -134,8 +255,7 @@ public class Operacion {
                 
                 for(Movimiento m : movimientos){
                 
-                    if(formato.format(movimientosOperacion.getFecha()).equals("01")){
-                    
+                    if(formato.format(movimientosOperacion.getFecha()).equals("08")){
                     
                         
                         
@@ -149,8 +269,7 @@ public class Operacion {
                 
                 for(Movimiento m : movimientos){
                 
-                    if(formato.format(movimientosOperacion.getFecha()).equals("01")){
-                    
+                    if(formato.format(movimientosOperacion.getFecha()).equals("09")){
                     
                         
                         
@@ -164,8 +283,7 @@ public class Operacion {
                 
                 for(Movimiento m : movimientos){
                 
-                    if(formato.format(movimientosOperacion.getFecha()).equals("01")){
-                    
+                    if(formato.format(movimientosOperacion.getFecha()).equals("10")){
                     
                         
                         
@@ -179,8 +297,7 @@ public class Operacion {
                 
                 for(Movimiento m : movimientos){
                 
-                    if(formato.format(movimientosOperacion.getFecha()).equals("01")){
-                    
+                    if(formato.format(movimientosOperacion.getFecha()).equals("11")){
                     
                         
                         
@@ -194,8 +311,7 @@ public class Operacion {
                 
                  for(Movimiento m : movimientos){
                 
-                    if(formato.format(movimientosOperacion.getFecha()).equals("01")){
-                    
+                    if(formato.format(movimientosOperacion.getFecha()).equals("12")){
                     
                         
                         
